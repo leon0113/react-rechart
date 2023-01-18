7345c6ad48e0ccb7a0c9861ded5ebdc97452d28d
leon0113 <tahjib.leon399@gmail.com>
2023-01-18 21:38:30 +0600

    react Chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MyChart from './components/MyChart/MyChart';
 
 function App() {
   return (
     <div className="App">
-      
+      <MyChart></MyChart>
     </div>
   );
 }
--- src/components/MyChart/MyChart.js
+++ src/components/MyChart/MyChart.js
@@ -1,54 +1,60 @@
 import React from 'react';
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const MyChart = () => {
     const data = [
         {
-          name: 'Page A',
-          uv: 4000,
+          name: 'product A',
+          price: 4000,
           pv: 2400,
           amt: 2400,
         },
         {
-          name: 'Page B',
-          uv: 3000,
+          name: 'product B',
+          price: 3000,
           pv: 1398,
           amt: 2210,
         },
         {
-          name: 'Page C',
-          uv: 1000,
+          name: 'product C',
+          price: 1000,
           pv: 800,
           amt: 2290,
         },
         {
-          name: 'Page D',
-          uv: 2780,
+          name: 'product D',
+          price: 2780,
           pv: 3908,
           amt: 2000,
         },
         {
-          name: 'Page E',
-          uv: 1890,
+          name: 'product E',
+          price: 1890,
           pv: 4800,
           amt: 2181,
         },
         {
-          name: 'Page F',
-          uv: 2390,
+          name: 'product F',
+          price: 2390,
           pv: 3800,
           amt: 2500,
         },
         {
-          name: 'Page G',
-          uv: 3490,
+          name: 'product G',
+          price: 3490,
           pv: 4300,
           amt: 2100,
         },
       ];
     return (
-        <div>
-            
-        </div>
+        <LineChart width={800} height={500} data={data}>
+            <Line type="monotone" dataKey="price" ></Line>
+            <Line dataKey="pv" ></Line>
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="name"></XAxis>
+            <YAxis></YAxis>
+            <Tooltip></Tooltip>
+        </LineChart>
     );
 };
 
