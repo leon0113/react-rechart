25e84e3cfcad373bdf8b70c1981a28e2347ebf2a
leon0113 <tahjib.leon399@gmail.com>
2023-01-18 22:44:20 +0600

    Piechart

--- src/components/AxiosChart/AxiosChart.js
+++ src/components/AxiosChart/AxiosChart.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, Cell, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const AxiosChart = () => {
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
     const [phones,setPhones] = useState([]);
     useEffect(()=>{
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -21,13 +22,35 @@ const AxiosChart = () => {
         })
     },[])
     return (
-        <BarChart width={800} height={300} data={phones}>
-          <Bar dataKey="value" fill="#8884d8" />
-          {/* <CartesianGrid stroke="#ccc" strokeDasharray="5 5" /> */}
-            <XAxis dataKey="name"></XAxis>
-            <YAxis></YAxis>
-            <Tooltip></Tooltip>
-        </BarChart>
+        // <div>
+        //     <BarChart width={800} height={300} data={phones}>
+        //   <Bar dataKey="value" fill="#8884d8" />
+        //   {/* <CartesianGrid stroke="#ccc" strokeDasharray="5 5" /> */}
+        //     <XAxis dataKey="name"></XAxis>
+        //     <YAxis></YAxis>
+        //     <Tooltip></Tooltip>
+        // </BarChart>
+        //  <br />
+         <PieChart width={400} height={400}>
+          <Pie
+            data={phones}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            // label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {phones.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+           
+          </Pie>
+          <Tooltip></Tooltip>
+        </PieChart>
+        // </div>
+        
     );
 };
 
