46eb6e8fed0a6a2d1bc7a5a6f4edca5ce45baaa4
leon0113 <tahjib.leon399@gmail.com>
2023-01-18 22:29:35 +0600

    AxiosChart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MyChart from './components/MyChart/MyChart';
+import AxiosChart from './components/AxiosChart/AxiosChart';
 
 function App() {
   return (
     <div className="App">
       <MyChart></MyChart>
+      <AxiosChart></AxiosChart>
     </div>
   );
 }
